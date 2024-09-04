55d79158a792cce012f00cfd9760d14dcfd65284
Taiwo Onileowo <taiwoonileowo17@gmail.com>
2024-09-04 11:55:39 +0100

    finish dashboard 3

--- components/Courses/AllCourses.tsx
+++ components/Courses/AllCourses.tsx
@@ -4,7 +4,7 @@ import Course from "../ui/Course";
 const AllCourses = ({ showEnrolled }: { showEnrolled: boolean }) => {
   return (
     <div className=" flex flex-col max-lg:items-center overflow-y-auto overflow-hidden scrollbar">
-      <div className="  py-8 px-4 md:grid md:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8">
+      <div className="flex flex-col  py-8 px-4 md:grid md:grid-cols-2 xl:grid-cols-3 gap-6 md:gap-8">
         <Course enrolled={showEnrolled} />
         <Course enrolled={showEnrolled} />
         <Course enrolled={showEnrolled} />
